a8bf18fe667d0a23bb05b7e9f9b8994a5dffee84
AlexSam <163metall163@gmail.com>
2022-06-09 12:51:49 +0400

    add Task3

--- Seminar1/Task3/Program.cs
+++ Seminar1/Task3/Program.cs
@@ -1,3 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет*/
 
+Console.WriteLine("Введите число");
+
+int x = Convert.ToInt32(Console.ReadLine());
+
+if (x % 2 ==0)
+{
+    Console.WriteLine("да, это число четное");
+}
+
+else
+{
+    Console.WriteLine("нет, это число не четное");
+}
