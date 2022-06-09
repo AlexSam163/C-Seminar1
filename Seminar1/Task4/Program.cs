99c93c56f1334ea1ed9b67df7fe1885928507671
AlexSam <163metall163@gmail.com>
2022-06-09 13:16:34 +0400

    add Task4

---
+++ Seminar1/Task4/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8*/
+Console.Clear();
+Console.WriteLine("Введите число");
+
+int N = Convert.ToInt32(Console.ReadLine ());
+
+int sum = 2;
+
+Console.WriteLine("Четные числа:");
+
+while (sum <= N)
+{
+Console.Write(sum + " ");
+sum += 2;
+}
+
+
+
+
+
